0bc577b21726fc8e99f9b03e3a34f4c39e187685
SaiEswar002 <saieswar2kg@gmail.com>
2025-08-20 11:25:02 +0530

    `Removed commented out link in Navbar component`

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = () => {
     { name: "Home", href: "#home" },
     { name: "About", href: "#about" },
     { name: "Projects", href: "#projects" },
-    { name: "Contact", href: "#contact" },
+    // { name: "Contact", href: "#contact" },
   ];
 
   const scrollToSection = (href: string) => {
